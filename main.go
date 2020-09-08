a7c46e2102f88bebdd485ef0a324233776bf1c84
unknown <tofig.bakhshiyev@ogr.sakarya.edu.tr>
2020-09-08 23:51:17 +0400

    add fibbonacci function

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+func Fibonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return Fibonacci(n-1) + Fibonacci(n-2)
+}
+
 func main() {
 	fmt.Println("Go actions")
+	answer := Fibonacci(9)
+	fmt.Println(answer)
 }
--- main_test.go
+++ main_test.go
@@ -6,5 +6,21 @@ import (
 )
 
 func TestPass(t *testing.T) {
-	fmt.Println("Placeholder")
+	var tests = []struct {
+		input    int
+		expected int
+	}{
+		{9, 34},
+		{5, 5},
+		{6, 8},
+		{11, 89},
+		{19, 4181},
+	}
+
+	for _, test := range tests {
+		if output := Fibonacci(test.input); output != test.expected {
+			t.Error("Test failed: output {}, expected {}", test.input, test.expected)
+		}
+	}
+	fmt.Println("Done")
 }
